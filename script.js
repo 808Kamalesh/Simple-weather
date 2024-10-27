bd175a888128e3cc5d66589e227aa9fc898d00bf
808Kamalesh <154341138+808Kamalesh@users.noreply.github.com>
2024-10-27 21:26:51 +0530

    updated

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getWeather() {
+    const city = document.getElementById('cityInput').value;
+    const apiKey = '8621a3f44413dc184e7aa3e17becac09';  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        if(data.cod === 200){
+            const weatherInfo = `
+                <h2>Weather in ${data.name}</h2>
+                <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
+                <p><strong>Weather:</strong> ${data.weather[0].description}</p>
+                <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+                <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+            `;
+
+            document.getElementById('weatherResult').innerHTML = weatherInfo;
+        } else {
+            document.getElementById('weatherResult').innerHTML = `<p>${data.message}</p>`;
+        }
+    } catch {
+        document.getElementById('weatherResult').innerHTML = `<p>Error. Please try again.</p>`;
+    }
+}
